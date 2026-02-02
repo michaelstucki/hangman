11b43dc97677ac28201ae6ef48c4dd6d79a419f4
michaelstucki <stcuthbertus@protonmail.com>
2026-02-02 11:59:36 -0700

    partially implement make_move functions and add tests

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -16,4 +16,13 @@ defmodule HangmanImplGameTest do
     assert game.game_state == :initializing
     assert game.letters == word |> String.codepoints
   end
+
+  test "state does not change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      {new_game, _tally} = Game.make_move(game, "x")
+      assert new_game == game
+    end
+  end
 end
